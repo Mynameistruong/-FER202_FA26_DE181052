7634ed7bab7f7fc478aab703d20bfd68834832e8
Truonglq <truonglq204@gmail.com>
2026-09-21 08:55:58 +0700

    Fix submodules for slot 3 and slot 5

---
+++ slot 3/my-first-app/About.jsx
@@ -0,0 +1,11 @@
+// src/About.jsx
+function About() {
+  return (
+    <div style={{ padding: '20px', border: '1px solid #ccc', marginTop: '10px' }}>
+      <h2>Trang giới thiệu (About)</h2>
+      <p>Chào mừng bạn đến với ứng dụng React của tôi!</p>
+    </div>
+  );
+}
+
+export default About;
