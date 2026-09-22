c2be468e9c69e6a2b40a9675ab415fe3948bd5b1
Truonglq <truonglq204@gmail.com>
2026-09-23 02:00:44 +0700

    chore(khoitao-lab2): bai tap es6 reactjs

--- .history/slot 5/baitap-es6-lab2/src/App_20260916110812.jsx
+++ .history/slot 5/baitap-es6-lab2/src/App_20260916110812.jsx
@@ -3,7 +3,6 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   const [count, setCount] = useState(0)
