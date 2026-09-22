58e1f1a12f418ac2ce109e474535c7bf767161cd
Truonglq <truonglq204@gmail.com>
2026-09-22 20:22:44 +0700

    chore(khoitao-lab2):bai tap es6 reactjs

---
+++ .history/slot 5/baitap-es6-lab2/src/App_20260921091251.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import WelcomeCard from './components/WelcomeCard';
+
+function App() {
+  return (
+    <div className="container my-4">
+      <WelcomeCard />
+    </div>
+  );
+}
+
+export default App;
